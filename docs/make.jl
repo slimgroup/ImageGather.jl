3a2c6bae08a2704edbfc58c3dcaee7e53e8cca37
Mathias Louboutin <mathias.louboutin@gmail.com>
2024-01-16 11:47:19 -0500

    fix doc

--- docs/make.jl
+++ docs/make.jl
@@ -1,11 +1,12 @@
 using Documenter, ImageGather, DocumenterCitations
 
-bib = CitationBibliography(joinpath(@__DIR__, "cig-refs.bib"), sorting = :nyt)
+bib = CitationBibliography(joinpath(@__DIR__, "cig-refs.bib"); style = :authoryear)
 
-makedocs(bib, sitename="Image gather tools",
+makedocs(;sitename="Image gather tools",
          doctest=false, clean=true,
          authors="Mathias Louboutin",
-         pages = Any["Home" => "index.md"])
+         pages = Any["Home" => "index.md"],
+         plugins=[bib])
 
 deploydocs(repo="github.com/slimgroup/ImageGather.jl",
            devbranch="main")
